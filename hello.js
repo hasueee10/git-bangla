226bda59f74933156947dd049070b1f6c7e967ee
Hasu <hasu.mridha@gmail.com>
2019-12-25 00:14:42 +0600

    Added a new method called greeting to hell.js

--- hello.js
+++ hello.js
@@ -2,4 +2,11 @@ class Hello {
   constructor(name) {
     this.name = name;
   }
+
+  greeting() {
+    console.log("Welcome " + this.name);
+  }
 }
+
+let sayWelcome = new Hello("Hasu");
+sayWelcome.greeting();
